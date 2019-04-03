c9f43c2e65498a97cbce4e5ddeb0a00176b0fe7c
dimkanevstruev <nevstruev.dmitriy@mail.ru>
2019-04-03 20:53:55 +0300

    adding send and avalabel

--- web_app/routes.py
+++ web_app/routes.py
@@ -1,15 +1,17 @@
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for, request, Flask
 from web_app import app, db
 from web_app.forms import LoginForm, RegistrationForm
 from web_app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
+from web_app.send import send
 
 @app.route('/')
 @app.route('/index')
 @login_required
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Miguel',
+            'avalabel': 'false'}
     posts = [
             {
                 'author': {'username': 'John'},
@@ -31,17 +33,24 @@ def login():
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
             return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
+        
+        login_user(user, remember = form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('index')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('login.html', title='Sign In', form = form)
 
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+@app.route('/users')
+def users():
+    users = User.query
+    return render_template('users.html', title='Users', posts=users)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -49,10 +58,17 @@ def register():
         return redirect(url_for('index'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
+        send(form.email.data, url_for('avalabel'))
+
+        user = User(username = form.username.data, email = form.email.data)
         user.set_password(form.password.data)
         db.session.add(user)
         db.session.commit()
         flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
+        return redirect(url_for('index'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/avalabel')
+def avalabel():
+    
+    return render_template('avalabel.html', title='avalabel')
