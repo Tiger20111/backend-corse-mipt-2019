32d478b4254b2bc9c9a85e0407eb5da15e77c633
Tiger20111 <nikit-proc@mail.ru>
2019-04-04 00:07:08 +0300

    Update send.py

--- web/web_app/send.py
+++ web/web_app/send.py
@@ -4,7 +4,7 @@ def send(email, url):
     conn_params = pika.ConnectionParameters('rabbit', 5672)
     connection = pika.BlockingConnection(conn_params)
     channel = connection.channel()
-    channel.queue_declare(queue='queue', durable=True)
+    channel.queue_declare(queue='first-queue', durable=True)
 
     message = email + ' ' + url
 
